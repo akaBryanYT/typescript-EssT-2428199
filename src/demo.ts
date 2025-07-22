94c7658b10a04361eafad1914d70f8a25eb7aa9f
bchavez <bryan.chavez@serco-na.com>
2025-07-22 11:07:14 -0400

    Progress

--- src/demo.ts
+++ src/demo.ts
@@ -1,13 +1,21 @@
 interface Contact {
-    id: number;
-    name: ContactName;
-    birthDate?: Date;
+  id: number;
+  name: ContactName;
+  birthDate?: Date;
+  status: ContactStatus;
+}
+
+enum ContactStatus {
+  Active = "Active ",
+  Inactive = "Inactive",
+  New = "New",
 }
 
 let primaryContact: Contact = {
-    birthDate: new Date("01-01-1980"),
-    id: 12345,
-    name: "Jamie Johnson",
+  birthDate: new Date("01-01-1980"),
+  id: 12345,
+  name: "Jamie Johnson",
+  status: ContactStatus.Inactive
 }
 
 type ContactName = string
